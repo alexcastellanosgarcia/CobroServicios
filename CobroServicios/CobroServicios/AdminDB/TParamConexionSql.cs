79f1015618360f1d9ff871a12caad005eeb0943a
Alex Castellanos Garcia <alexcastellanosgarcia@plastinova.com>
2022-03-26 20:58:59 -0600

    Agregando Parametros de Conexion  a base de datos SQL.

---
+++ CobroServicios/CobroServicios/AdminDB/TParamConexionSql.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CobroServicios.AdminDB
+{
+    class TParamConexionSql
+    {
+        private SqlConnectionStringBuilder ConectorSql = new SqlConnectionStringBuilder();
+
+        public TParamConexionSql()
+        {
+            ConectorSql.DataSource = "DESKTOP-27BVAGI";
+            ConectorSql.InitialCatalog = "COBROSERVICIOS";
+            ConectorSql.UserID = "sa";
+            ConectorSql.Password = "Admin%123";
+
+
+        }
+
+    }
+}
---
+++ CobroServicios/CobroServicios/Clientes/TModelado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobroServicios.Clientes
+{
+    public class TModelClientes
+    {
+        int Id { set; get; }
+        String Identidad { set; get; }
+        String Nombre { set; get; }
+        String Direccion { set; get; }
+        String Telefono { set; get; }
+        String Correo { set; get; }
+        String Municipio { set; get; }
+        String FechaNacimiento { set; get; }
+        String FechaCreacion { set; get; }
+        String FechaModificacion { set; get; }
+        String UsuarioCreacion { set; get; }
+        String UsuarioModificacion { set; get; }
+
+    }
+}
